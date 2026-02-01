0f908bfff3d938277439ef10592c282300d513cf
ardhanfabiyan <ardhanfabiyan@gmail.com>
2026-02-02 01:53:38 +0700

    Add files via upload

---
+++ peminjam/katalog.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PEAK ZONE ADVENTURE</title>
+    <link rel="stylesheet" href="styleadmin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
+    integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+    <nav>
+        <div class="logo">
+            <a href="indexadmin.php"><img src="ukksewa.png"></a>
+        </div>
+        <ul class="navbar">
+        <li><a href="indexpeminjam.php" class="active">Home</a></li>
+        <li><a href="katalog.php" class="active">Katalog</a></li>
+        <li><a href="layanan.php">Syarat & Ketentuan</a></li>
+        <li><a href="logout.php" class="active">Logout</a></li>
+        </ul>
+    </nav>
+    </header>
+    <section id="kepala">
+    <section class="feature">
+        <div class="middle-text">
+            <h1 align=center>*Katalog Perlengkapan Hiking</h1>
+            <!-- <p>Jelajahi koleksi perlengkapan hiking kami yang lengkap dan berkualitas tinggi untuk mendukung petualangan alam Anda. 
+                Dari tenda hingga sepatu hiking, temukan semua yang Anda butuhkan di sini.</p> -->
+        </div>
+        <div class="feature-content">
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="main-row">
+                    <div class="row-image">
+                        <img src="gambar/download.jfif">
+                        <div class="row-text">
+                        <h2>Jaket Gorpcore</h2>
+                        <span class="warna">Rating: <i class="fa-solid fa-star"></i>(x5)</span><br><br>
+                        <button><a href="finputpinjam.php" class="row-button">Detail</a></button>
+                        <button><a href="finputpinjam.php" class="row-button">Sewa <i class="fa-solid fa-cart-arrow-down"></i></a></button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </section>
+</section>
+     <div id="image-modal" class="modal">
+        <span class="close-button">&times;</span>
+        <img class="modal-content" id="zoomed-image">
+    </div>
+     <script src="script.js"></script>
+    <?php
+    include "footer.php";
+    ?>
+</body>
+</html>
